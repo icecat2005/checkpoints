43988645bf047632dbad551007cf315d67a1b6f2
icecat2005 <39443757+icecat2005@users.noreply.github.com>
2020-05-26 13:57:57 +0100

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -6,17 +6,22 @@ from playsound import playsound
 hour_audio = "dingding.wav by ljudman.mp3"
 checkpoint_audio = "ShipsBell.wav by acclivity.mp3"
 interval = 15
+current_time = time.localtime(time.time())
+current_min = current_time[4]
+current_sec = current_time[5]
+
+
 
 while True:
-	current_time = time.localtime(time.time())
-	current_min = current_time[4]
-	current_sec = current_time[5]
 
 	min_to_checkpoint = (interval-1) - current_min % interval
 	sec_to_checkpoint = 60 - current_sec % 60
 
 	time.sleep(min_to_checkpoint * 60 + sec_to_checkpoint)
 
+	current_time = time.localtime(time.time())
+	current_min = current_time[4]
+	current_sec = current_time[5]
 	if current_time[4] == 0:
 		playsound(hour_audio)
 	else:
