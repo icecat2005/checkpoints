f974cbb9345a85b12553ebef19f5601277f332ab
icecat2005 <39443757+icecat2005@users.noreply.github.com>
2020-05-26 13:46:35 +0100

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -2,15 +2,18 @@ import time
 from playsound import playsound
 from pydub import AudioSegment
 from pydub.playback import play
+
+# User Set Up
 hour_audio = "dingding.wav by ljudman.mp3"
 checkpoint_audio = "ShipsBell.wav by acclivity.mp3"
+interval = 15
 
 while True:
 	current_time = time.localtime(time.time())
 	current_min = current_time[4]
 	current_sec = current_time[5]
 
-	min_to_checkpoint = 14 - current_min % 15
+	min_to_checkpoint = (interval-1) - current_min % interval
 	sec_to_checkpoint = 60 - current_sec % 60
 
 	time.sleep(min_to_checkpoint * 60 + sec_to_checkpoint)
